c151b41a401148a8b6a771ece15429ffbf8969fa
Sharath Bhaskara <sbhaskara@connectall.com>
2021-09-01 22:43:11 -0700

    added a variable

--- src/test/java/com/connectall/adapter/zabbix/controller/VulnerabilitiesTest.java
+++ src/test/java/com/connectall/adapter/zabbix/controller/VulnerabilitiesTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class VulnerabilitiesTest {
 	
+	public final String TEST_VAR = "1";
+	
 	public static void main(String[] aa) throws IOException {
 		String cmd = new String("cmd.exe /K \"c:\\util\\rmanDB.bat "
 		+aa[0]+
