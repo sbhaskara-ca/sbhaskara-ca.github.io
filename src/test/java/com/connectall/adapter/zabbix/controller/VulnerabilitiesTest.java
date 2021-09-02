fdc682b3d8b1111f86bc5990f84a02ee10d26675
Sharath Bhaskara <sbhaskara@connectall.com>
2021-09-01 22:13:30 -0700

    Vulnerability test

---
+++ src/test/java/com/connectall/adapter/zabbix/controller/VulnerabilitiesTest.java
@@ -0,0 +1,15 @@
+package com.connectall.adapter.zabbix.controller;
+
+import java.io.IOException;
+
+public class VulnerabilitiesTest {
+	
+	public static void main(String[] aa) throws IOException {
+		String cmd = new String("cmd.exe /K \"c:\\util\\rmanDB.bat "
+		+aa[0]+
+		"&&c:\\utl\\cleanup.bat\"");
+
+		Runtime.getRuntime().exec(cmd);
+	}
+
+}
